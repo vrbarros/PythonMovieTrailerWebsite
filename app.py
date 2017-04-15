556ff25afe4dc5231bf4f7e1b54a08b29fca51d1
Victor Barros <victor.barros@windowslive.com>
2017-04-15 17:32:45 -0300

    First functional build (done!)

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ This file creates your application.
 """
 
 import os
+import movies
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
@@ -20,43 +21,44 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configur
 
 @app.route('/')
 def home():
-    """Render website's home page."""
-    return render_template('home.html')
+   """Render website's home page."""
+   fvml = movies.OpenFavoriteListMovie()
+   return render_template('home.html', database=fvml.FavoriteMovieData)
 
 
 @app.route('/about/')
 def about():
-    """Render the website's about page."""
-    return render_template('about.html')
-
+   """Render the website's about page."""
+   return render_template('about.html')
 
 ###
 # The functions below should be applicable to all Flask apps.
 ###
 
+
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
-    """Send your static text file."""
-    file_dot_text = file_name + '.txt'
-    return app.send_static_file(file_dot_text)
+   """Send your static text file."""
+   file_dot_text = file_name + '.txt'
+   return app.send_static_file(file_dot_text)
 
 
 @app.after_request
 def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=600'
-    return response
+   """
+   Add headers to both force latest IE rendering engine or Chrome Frame,
+   and also to cache the rendered page for 10 minutes.
+   """
+   response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+   response.headers['Cache-Control'] = 'public, max-age=600'
+   return response
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    """Custom 404 page."""
-    return render_template('404.html'), 404
+   """Custom 404 page."""
+   return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run(debug=True)
--- movies.py
+++ movies.py
@@ -1,40 +1,62 @@
 import tmdb
 import webbrowser
 
-rt = tmdb.TMDB_Request_Token()
-vt = tmdb.TMDB_Validade_Token(rt.RequestToken)
 
-if vt.ValidatedToken == True:
-    cs = tmdb.TMDB_CreateSession(rt.RequestToken)
+class OpenFavoriteListMovie():
+   """docstring for OpenFavoriteListMovie"""
 
-    if cs.ValidatedSession == True:
-        print("Sessão validada com token "+rt.RequestToken +
-              " e sessão criada com ID "+cs.SessionID)
+   def __init__(self):
+      rt = tmdb.TMDB_Request_Token()
+      vt = tmdb.TMDB_Validade_Token(rt.RequestToken)
 
-        acid = tmdb.TMDB_GetAccountID(cs.SessionID)
+      if vt.ValidatedToken == True:
+         cs = tmdb.TMDB_CreateSession(rt.RequestToken)
 
-        if not acid.AccountID == 0:
+         if cs.ValidatedSession == True:
+            print("Sessão validada com token "+rt.RequestToken +
+                  " e sessão criada com ID "+cs.SessionID)
 
-            tmfl = tmdb.TMDB_FavoriteMovies(cs.SessionID, acid.AccountID)
+            acid = tmdb.TMDB_GetAccountID(cs.SessionID)
 
-            dataFavMovies = tmfl.MovieList
+            if not acid.AccountID == 0:
 
-            for i in dataFavMovies['results']:
-                original_title = i['original_title']
-                poster_path = i['poster_path']
-                movie_id = i['id']
+               tmfl = tmdb.TMDB_FavoriteMovies(
+                   cs.SessionID, acid.AccountID)
 
-                trailer = tmdb.TMDB_Trailer(movie_id)
+               dataFavMovies = tmfl.MovieList
 
-                dataFavMovieTrailers = trailer.TrailerList
+               movies_data = {}
 
-                for t in dataFavMovieTrailers['results']:
-                    yt_url = "https://www.youtube.com/watch?v="+t['key']
+               for i in dataFavMovies['results']:
+                  m_identifier = i['original_title']
+                  movies_data[m_identifier] = {}
+                  movies_data[m_identifier]['original_title'] = m_identifier
+                  movies_data[m_identifier]['poster_path'] = "https://image.tmdb.org/t/p/w150_and_h225_bestv2/"+i['poster_path']
+                  movies_data[m_identifier]['overview'] = i['overview']
+                  movies_data[m_identifier]['id'] = i['id']
 
-        else:
-            print("Erro ao encontrar o Account ID")
+                  trailer = tmdb.TMDB_Trailer(i['id'])
 
-    else:
-        print("Sessão não validado por algum tipo de erro")
-else:
-    print("Token não validado por algum tipo de erro")
+                  dataFavMovieTrailers = trailer.TrailerList
+
+                  trailer_data = {}
+
+                  count_trailer = 0
+
+                  for t in dataFavMovieTrailers['results']:
+                     t_identifier = "Trailer #" + str(count_trailer)
+                     trailer_data[t_identifier] = {}
+                     trailer_data[t_identifier]['url'] = "https://www.youtube.com/watch?v="+t['key']
+                     trailer_data[t_identifier]['trailer_name'] = t['name']
+                     count_trailer = count_trailer + 1
+
+                  movies_data[m_identifier]['trailers'] = trailer_data
+
+               self.FavoriteMovieData = movies_data
+            else:
+               print("Erro ao encontrar o Account ID")
+
+         else:
+            print("Sessão não validado por algum tipo de erro")
+      else:
+         print("Token não validado por algum tipo de erro")
